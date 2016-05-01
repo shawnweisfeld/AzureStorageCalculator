3fefeb87d37f45a4ba99098dccc885815c3a0b8d
Shawn Weisfeld <shawn@shawnweisfeld.com>
2016-05-01 17:17:02 -0500

    added AI and fork link

--- AzureStorageCalculator/App_Start/FilterConfig.cs
+++ AzureStorageCalculator/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace AzureStorageCalculator
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new AiHandleErrorAttribute());
         }
     }
 }
